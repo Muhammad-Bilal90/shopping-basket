ff64150ab2b6d8059b88ec98d11da44c795ac388
Muhammad Bilal <bkafridi9833@gmail.com>
2022-05-16 15:12:06 +0500

    created 404 page

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,6 +1,7 @@
 import NavigationBar from "./Components/Navbar/navbar";
 import Home from "./Components/Home/home";
 import Cart from "./Components/Cart/cart";
+import NotFound from "./Components/NotFound/notFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Routers = () => {
@@ -10,6 +11,7 @@ const Routers = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
