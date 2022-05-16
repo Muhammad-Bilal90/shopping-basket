7f6a3a316fd47107beeddae52f4a1e053f2b4d08
Muhammad Bilal <bkafridi9833@gmail.com>
2022-05-16 15:26:56 +0500

    Update Routers.tsx

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,7 +1,6 @@
 import NavigationBar from "./Components/Navbar/navbar";
 import Home from "./Components/Home/home";
 import Cart from "./Components/Cart/cart";
-import NotFound from "./Components/NotFound/notFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Routers = () => {
@@ -11,7 +10,6 @@ const Routers = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="cart" element={<Cart />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
